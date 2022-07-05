4ef7530ab63ac09549513aadcba38868eb152fb0
Amandeep <singh.amandeep5665@gmail.com>
2022-07-05 19:05:05 +0530

    navbar done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,28 +1,63 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Link from 'next/link'
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {AiFillCloseCircle} from 'react-icons/ai';
 
 const Navbar = () => {
+
+  const [nav,setNav] = useState(true)
+
+  const handleNav =() =>{
+      setNav(!nav)
+  }
   return ( 
-    <div>
-        <div>
-            <Link href = '/'>
-            <h1>Rishav</h1> 
+    <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+        <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
+            <Link href = '/'> 
+            <h1 className='font-bold text-4xl'>Rishav</h1> 
             </Link>
             
-            <ul>
-                <li>
+            <ul className='hidden sm:flex'>
+                <li className='p-4'>
                     <Link href = '/'>Home</Link>
                 </li>
-                <li>
+                <li className='p-4'>
                     <Link href = '/#gallery'>Gallery</Link>
                 </li>
-                <li>
+                <li className='p-4'>
                     <Link href = '/portfolio'>Work</Link>
                 </li>
-                <li>
+                <li className='p-4'>
                     <Link href = '/contact'>Contact</Link>
                 </li>
             </ul>
+
+            {/* Mobile Button */}
+
+            <div className='block sm:hidden z-10'>
+                {nav ? <AiFillCloseCircle size={20} /> : <GiHamburgerMenu size={20} />}
+                </div>
+                {/* Mobile Menu */}
+                
+                <div className= {nav? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black/80 text-center ease-in duration-300' : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black/50 text-center ease-in duration-300'}>
+                    <ul>
+
+                    <li className='p-4 text-4xl hover:text-gray-500'>
+                    <Link href = '/'>Home</Link>
+                </li>
+                <li className='p-4 text-4xl hover:text-gray-500'>
+                    <Link href = '/#gallery'>Gallery</Link>
+                </li>
+                <li className='p-4 text-4xl hover:text-gray-500'>
+                    <Link href = '/portfolio'>Work</Link>
+                </li>
+                <li className='p-4 text-4xl hover:text-gray-500'>
+                    <Link href = '/contact'>Contact</Link>
+                </li>
+                    </ul>
+                
+            </div>
+
         </div>
     </div>
   )
